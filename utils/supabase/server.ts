b2e060c34bfb3424404432ec091efe5f2ccced5c
Brock Benson <brockjbenson@outlook.com>
2024-12-31 11:18:21 -0600

    Update env vars to connect to supabase

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -5,8 +5,8 @@ export const createClient = async () => {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.DB_NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.DB_NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
@@ -24,6 +24,6 @@ export const createClient = async () => {
           }
         },
       },
-    },
+    }
   );
 };
