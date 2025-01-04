409f75f0fe6d6dcb76f698fc59926a3e0dc5e1f1
Brock Benson <brockjbenson@outlook.com>
2025-01-03 19:02:54 -0600

    add down for maintenance

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import Header from "@/components/header";
+import Logo from "@/components/logo";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -30,8 +31,12 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
-          disableTransitionOnChange>
-          {children}
+          disableTransitionOnChange
+        >
+          <div className="h-screen w-screen flex flex-col items-center gap-20 justify-center">
+            <Logo size="xl" />
+            <h1>Coming Soon</h1>
+          </div>
         </ThemeProvider>
       </body>
     </html>
